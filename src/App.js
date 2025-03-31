e5b8ca7bb83e22c48e15aa7a67b9d660b03cd566
SindhooraO <sindhooraodiyoor@gmail.com>
2025-03-31 13:58:23 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css"
 
-function App() {
+const TodoList = () => {
+  const [activity, setActivity] = useState("");
+  const [tasks, setTasks] = useState([]);
+
+  const addActivity = () => {
+    if (activity) {
+      setTasks([...tasks, activity]);
+      setActivity(""); 
+    }
+  };
+
+  const deleteActivity = (index) => {
+    const newTasks = [...tasks]; // Create a copy of the array
+    newTasks.splice(index, 1); // Remove 1 item at the given index
+    setTasks(newTasks); // Update the state
+  };
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="container">
+    <h2>To-Do List</h2>
+    <input type="text" value={activity} onChange={(e) => setActivity(e.target.value)} />
+    <button onClick={addActivity}>ADD</button>
+    <ul>
+      {tasks.map((task, index) => (
+        <li key={index}>
+          {task} <button onClick={() => deleteActivity(index)}>DELETE</button>
+        </li>
+      ))}
+    </ul>
+  </div>
+  
   );
-}
+};
 
-export default App;
+export default TodoList;
+
